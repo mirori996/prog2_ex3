52930ee5f7915390d1e6f96d3534f256a9e41e84
Tamahashi Yutaro <e175743@ie.u-ryukyu.ac.jp>
2017-12-05 11:52:53 +0900

    MainにExDiceの内容を追加

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import jp.ac.uryukyu.ie.ex3.pair08.Dice;
+import jp.ac.uryukyu.ie.ex3.pair08.ExDice;
 
 public class Main {
     public static void main(String[] args){
         Dice dice = new Dice();
         dice.play();
         System.out.println(dice.getValue());
+        
+        ExDice exdice = new ExDice(5,7);
+        for (int i = 0; i < 10; i++) {
+            exdice.play();
+            System.out.println(exdice.getValue());
+        }
     }
 }
